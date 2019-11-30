4d85f9e5345bb1b5eb5321b3696799f3399a9c73
Yo Yi <colinyoyo26@gmail.com>
2019-11-30 21:10:44 +0800

    Fix path

--- tests/plot.sh
+++ tests/plot.sh
@@ -2,7 +2,7 @@
 for i in `seq 10 2000 100000`
 do
     printf "$i "
-    ../bench $i 
+    ./bench $i 
 done >> data.txt
 
-gnuplot plotscript.gp && rm *.txt
+gnuplot tests/plotscript.gp && rm *.txt
