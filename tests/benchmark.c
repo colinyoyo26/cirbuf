a4f39409a57948db21f35a4b12b603d347b427c8
Yo Yi <colinyoyo26@gmail.com>
2019-12-01 01:56:19 +0800

    Fix induction var type and function name

--- tests/benchmark.c
+++ tests/benchmark.c
@@ -8,7 +8,7 @@
 #define BUFFER_SIZE 1024
 #define MESSAGE_SIZE 32
 
-static inline double macrotime() {
+static inline double microtime() {
     struct timespec tv;
     clock_gettime(CLOCK_MONOTONIC_RAW, &tv);
     return tv.tv_nsec * 1e-3;
@@ -20,9 +20,9 @@ int main(int argc, char** argv) {
     size_t offset;
     size_t runs = atoi(argv[1]);
  
-    long slow_start = macrotime();
+    long slow_start = microtime();
     offset = 0;
-    for(int i = 0; i < runs; i++){
+    for(size_t i = 0; i < runs; i++){
         long mask = (MESSAGE_SIZE - BUFFER_SIZE + offset) >> 63;
         const size_t part1 = MESSAGE_SIZE & mask + (BUFFER_SIZE - offset) & ~mask;
         const size_t part2 = MESSAGE_SIZE - part1;
@@ -30,16 +30,16 @@ int main(int argc, char** argv) {
         memcpy(buffer, message + part1, part2);
         offset = (offset + MESSAGE_SIZE) % BUFFER_SIZE;
     }
-    long slow_stop = macrotime();
+    long slow_stop = microtime();
  
     cirbuf_t cb;
     cirbuf_new(&cb, BUFFER_SIZE);
-    long fast_start = macrotime();
-    for(int i = 0; i < runs; i++){
+    long fast_start = microtime();
+    for(size_t i = 0; i < runs; i++){
        cirbuf_offer(&cb, message, MESSAGE_SIZE);
        cirbuf_poll(&cb, MESSAGE_SIZE);
     }
-    long fast_stop = macrotime();
+    long fast_stop = microtime();
  
     printf("%ld %ld\n", (slow_stop - slow_start), (fast_stop - fast_start));
  
