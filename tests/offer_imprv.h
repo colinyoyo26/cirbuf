0f2b00c15071df0e4d994672511e1e1cbe20fffd
Yo Yi <colinyoyo26@gmail.com>
2019-12-08 21:11:12 +0800

    Optimize data cpy perf
    Perform binary search on remaining data
    Drease num of dereference

--- tests/offer_imprv.h
+++ tests/offer_imprv.h
@@ -1,4 +1,5 @@
 #include <immintrin.h>
+#include <stdint.h>
 
 static inline int cirbuf_offer_opt(cirbuf_t *cb,
                                    const unsigned char *data,
@@ -8,14 +9,33 @@ static inline int cirbuf_offer_opt(cirbuf_t *cb,
     if (cirbuf_unusedspace(cb) <= size)
         return 0;
 
+    void *base = cb->data + cb->tail;
     int i = 0, align = sizeof(__m256i);
     for(; i < (size & -align); i += align){
         __m256i buf = _mm256_lddqu_si256((__m256i  *) (data + i));
-        _mm256_storeu_si256((__m256i *) (cb->data + cb->tail + i), buf);
+        _mm256_storeu_si256((__m256i *) (base + i), buf);
     }
-
-    memcpy(cb->data + cb->tail + i, data + i, size & (align -1));
-
+    
+    if(size & 16){ 
+        __m128i buf2 = _mm_lddqu_si128 ((__m128i *) (data + i));
+        _mm_storeu_si128((__m128 *) (base + i), buf2);
+        i += 16;
+    }
+    if(size & 8){
+        *(int64_t *) (base + i) = *(int64_t *) (data + i);
+        i += 8;
+    }
+    if(size & 4){
+        *(int32_t *) (base + i) = *(int32_t *) (data + i);
+        i += 4;
+    }
+    if(size & 2){
+        *(int16_t *) (base + i) = *(int16_t *) (data + i);
+        i += 2;
+    }
+    if(size & 1)
+        *(int8_t *) (base + i) = *(int8_t *) (data + i);
+    
     cb->tail += size;
     if (cb->size < cb->tail)
         cb->tail -= cb->size;
